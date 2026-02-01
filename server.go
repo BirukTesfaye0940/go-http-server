5db8df3d6984a6a75e5b11e8a86c26357bff7e79
Biruk Tesfaye <biruktesfayeakabu@gmail.com>
2026-02-01 14:10:43 +0300

    feat: Add and apply a logging middleware to log incoming HTTP requests and their completion times.

--- server.go
+++ server.go
@@ -6,7 +6,23 @@ import (
 
 	"go-http-server/handlers"
 	"go-http-server/storage"
+	"log"
 )
+
+func LoggingMiddleware(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		start := time.Now()
+
+		// 1. Code here runs BEFORE the handler
+		log.Printf("Started %s %s", r.Method, r.URL.Path)
+
+		// 2. Pass the request to the real handler
+		next.ServeHTTP(w, r)
+
+		// 3. Code here runs AFTER the handler finishes
+		log.Printf("Completed %s in %v", r.URL.Path, time.Since(start))
+	})
+}
 
 func NewServer() *http.Server {
 	repo := storage.NewUserRepository()
@@ -15,9 +31,12 @@ func NewServer() *http.Server {
 	mux := http.NewServeMux()
 	mux.HandleFunc("/users", userHandler.Users)
 
+	var handler http.Handler = mux
+	handler = LoggingMiddleware(handler)
+
 	return &http.Server{
 		Addr:         ":8080",
-		Handler:      mux,
+		Handler:      handler,
 		ReadTimeout:  5 * time.Second,
 		WriteTimeout: 10 * time.Second,
 	}
